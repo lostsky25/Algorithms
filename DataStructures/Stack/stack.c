6404c41c37c158ba7180985301c503c6c893d4d7
lostsky25 <lostsky25@yahoo.com>
2023-11-15 00:12:45 +0300

    print function

--- DataStructures/Stack/stack.c
+++ DataStructures/Stack/stack.c
@@ -8,10 +8,19 @@ typedef struct {
     unsigned int n;
 } Stack;
 
-int main() {
+void print(Stack* st) {
+    for (unsigned int i = 0; i < st->n; ++i) {
+        printf("%d ", st->a[i]);
+    }
+    printf("\n");
+}
 
+
+int main() {
     Stack s = {{7, 4, 1}, 3};
     Stack* st = &s;
+
+    print(st);
 
     return 0;
 }
