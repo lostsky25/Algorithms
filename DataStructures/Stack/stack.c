adc6b98ca8241bee5834101ba1c4720b12ccdee2
lostsky25 <lostsky25@yahoo.com>
2023-11-15 00:21:16 +0300

    pop stack, pretty tests

--- DataStructures/Stack/stack.c
+++ DataStructures/Stack/stack.c
@@ -24,6 +24,12 @@ void push(Stack* st, Data data) {
     st->n++;
 }
 
+Data pop(Stack* st) {
+    Data res = st->a[st->n - 1];
+    st->n--;
+    return res;
+}
+
 int main() {
     Stack s = {{7, 4, 1}, 3};
     Stack* st = &s;
@@ -31,12 +37,32 @@ int main() {
     init(st);
     print(st);
 
+    Data d;
+    d = 5;
+    printf("push %d :", d);
     push(st, 5);
     print(st);      // 5
+
+    d = 17;
+    printf("push %d :", d);
     push(st, 17);
     print(st);      // 5 17
-    push(st, -3);
+
+    d = -3;
+    printf("push %d :", d);
+    push(st, d);
     print(st);      // 5 17 -3
 
+    d = pop(st);
+    printf("pop %d :", d);
+    print(st);      // pop -3: 5 17
+    
+    d = pop(st);
+    printf("pop %d :", d);
+    print(st);      // pop 17: 5
+    
+    d = pop(st);
+    printf("pop %d :", d);
+    print(st);      // pop 5:
     return 0;
 }
